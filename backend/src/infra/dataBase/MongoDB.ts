532f93c1a9227cae73980cffeece4a5284430ee6
lucas selliach <lucas.selliach@outlook.com>
2025-07-06 22:14:34 -0300

    feat: update docker to run backend

--- backend/src/infra/dataBase/MongoDB.ts
+++ backend/src/infra/dataBase/MongoDB.ts
@@ -3,7 +3,9 @@ import * as mongoose from 'mongoose';
 export class DatabaseConfig {
     static async connect() {
         console.log('Connection to Database');
-        await mongoose.connect('mongodb://root:MongoDB!@localhost:27017/?retryWrites=true&w=majority');
+        // await mongoose.connect('mongodb://root:MongoDB!@localhost:27017/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb://root:MongoDB!@mongo:27017/?retryWrites=true&w=majority');
+        // await mongoose.connect(process.env.MONGO_URL as string);
         console.log('Database successfully connected!');
     }
 }
--- backend/src/infra/http/appServer.ts
+++ backend/src/infra/http/appServer.ts
@@ -2,7 +2,7 @@ import { Express } from 'express';
 
 export class AppServer {
     static async init(app: Express) {
-        app.listen(8000, () => {
+        app.listen(8080, () => {
             console.log(`[App]: Server listening on 8000. 🚀`)
         });
     }
