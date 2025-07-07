64bd5f94acec7702805aa506c3f4a5e470708698
lucas selliach <lucas.selliach@outlook.com>
2025-07-06 23:39:14 -0300

    fix: change initial project to --no-standalone

---
+++ frontend/src/app/app-module.ts
@@ -0,0 +1,23 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,10 +1,18 @@
+import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        App
+      ],
+      providers: [provideZonelessChangeDetection()]
     }).compileComponents();
   });
 
